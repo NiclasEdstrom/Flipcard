26a473a1d9cc52e683ebdf6d63a17c9455c7181a
NiclasEdstrom <niclas_edstrom@hotmail.com>
2022-12-08 09:47:12 +0100

    Update
    dwd

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-const card = document.querySelector(".card__inner");
+const card = document.getElementsByClassName("card__inner");
 
 card.addEventListener("click", function (e) {
-  card.classList.toggle('is-flipped');
+  card.classList.toggle('is-flipped')[1];
 });
+
+function ChoseCard(cardNumber) 
+{
+  card[cardNumber].classList.toggle("is-flipped")
+}
